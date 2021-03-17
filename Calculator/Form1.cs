1fa51c498148a08089e6f42bd68087ca85092f75
Meera <47111883+MeeraSahu@users.noreply.github.com>
2021-03-17 21:56:53 +0530

    initial build
    An attempt to implement the basic functionality of windows calculator

---
+++ Calculator/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Calculator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Calculator(object sender, EventArgs e)
+        {
+
+        }
+
+        private void NumericData(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_BindingContextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
